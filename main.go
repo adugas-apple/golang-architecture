d5afafd0fd09b8d2266a5f5cdf12e4434addceee
Andrew Dugas <68975692+adugas-apple@users.noreply.github.com>
2021-11-05 16:28:05 -0700

    James Bond

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+type person struct {
+	first string
+}
+
+func (p person) speak () {
+	fmt.Println("from a person -- this is my name", p.first)
+}
+
+type human interface {
+	speak()
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+	}
+
+	fmt.Printf("%T\n", p1)
+
+	var x human
+	x = p1
+	x.speak()
+	fmt.Printf("%T\n", x)
+}
+
+/*
 //go:noinline
 func foo() int {
 	x := 42
@@ -16,3 +44,4 @@ func main() {
 	_ = foo()
 	_ = bar()
 }
+*/
